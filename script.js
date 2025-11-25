0e6e21e5adb50b84c086ef1a067ff141fca92ef2
genius-el <elvismichael888@gmail.com>
2025-11-25 07:20:19 +0100

    Add popup update feature using DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const update = document.querySelector('.update');
+const overlay = document.querySelector('.overlay');
+const btnCloseUpdate = document.querySelector('.close-update');
+const btnOpenUpdate = document.querySelector('.show-update');
+console.log(update, overlay, btnCloseUpdate, btnOpenUpdate);
+
+const openUpdate = function() {
+    update.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeUpdate = function() {
+    update.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnOpenUpdate.addEventListener('click', openUpdate);
+
+btnCloseUpdate.addEventListener('click', closeUpdate);
+overlay.addEventListener('click', closeUpdate);
+
+document.addEventListener('keydown', function(e) {
+    console.log(e.key);
+
+    if (e.key === 'Escape' && !update.classList.contains('hidden')) {
+        closeUpdate()
+    }
+})
